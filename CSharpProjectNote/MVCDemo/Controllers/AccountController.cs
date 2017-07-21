94cb8c06a5272c6766fc45186a9c4dfd8ce20b78
angrykun <2545662859@qq.com>
2017-07-21 18:43:10 +0800

    新增登录页面

--- CSharpProjectNote/MVCDemo/Controllers/AccountController.cs
+++ CSharpProjectNote/MVCDemo/Controllers/AccountController.cs
@@ -13,5 +13,33 @@ namespace MVCDemo.Controllers
         {
             return View();
         }
+
+        #region 登录
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            return View();
+        }
+        #endregion
+
+        #region 注册  
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Register()
+        {
+            return View();
+        }
+        #endregion
+
     }
 }
--- CSharpProjectNote/MVCDemo/README.md
+++ CSharpProjectNote/MVCDemo/README.md
@@ -1,7 +1,13 @@
 # MVCDemo+EF
 ---
-###一、MVC典型执行过程
+一、MVC典型执行过程
 
  1. 网址路由比对
  2. 如路由地址比对成功，执行相应的Controller与Action
- 3. 执行相应的View并返回结果
+ 3. 执行相应的View并返回结果  
+
+ 二、
+	1.禁用VS Browser Link =>在webconfig的appSettings节点中新增<add key="vs:EnableBrowserLink" value="false"/>  
+	2.[HttpPost]表示这个Action只会结束http post 请求。应用场景：涉及到需要接受客户端窗口数据时，创建一个用于接收
+		Http Get请求的Action 用于显示界面，提供给用户填写数据；另一个同名Action应用Http Post属性，用于接收用户发来
+		的数据，完成对应的功能。
