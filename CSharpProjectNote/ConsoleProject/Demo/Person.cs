b1c4eab9421baf112e90e9bd32740d02a0476394
angrykun <2545662859@qq.com>
2017-08-31 09:56:51 +0800

    深拷贝浅拷贝

---
+++ CSharpProjectNote/ConsoleProject/Demo/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject.Demo
+{
+    class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Job { get; set; }
+
+        protected string Say()
+        {
+            return "Hello ";
+        }
+    }
+}
