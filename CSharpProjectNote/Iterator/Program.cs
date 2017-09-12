7f90d6e31ee7c6f9eb059cddd9fa37765f856d6a
angrykun <2545662859@qq.com>
2017-09-12 19:16:42 +0800

    迭代器模式-未完成

---
+++ CSharpProjectNote/Iterator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    /**
+     * 迭代器模式：提供一种方法来访问聚合对象，
+     * 而不用暴露这个对象内部表示，其别名为游标（Cursor）
+     * 
+     * **/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
