b48a9268a4c17f907d1e71acfe5aac74e92dffe4
angrykun <2545662859@qq.com>
2017-09-15 17:05:20 +0800

    中介者模式--完成

---
+++ CSharpProjectNote/Mediator/LearningHard/AbstractCardPartnerGrade.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator.LearningHard
+{
+    public abstract class AbstractCardPartnerGrade
+    {
+        public int Moneny { get; set; }
+        public AbstractCardPartnerGrade()
+        {
+            Moneny = 0;
+        }
+        public abstract void ChangeCount(int count, AbstractMediator other);
+    }
+}
---
+++ CSharpProjectNote/Mediator/LearningHard/ParterAGrade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator.LearningHard
+{
+    /// <summary>
+    /// 具体牌友A（使用中介者设计模式）
+    /// </summary>
+    public class ParterAGrade : AbstractCardPartnerGrade
+    {
+        public override void ChangeCount(int count, AbstractMediator other)
+        {
+            other.AWin(count);
+        }
+    }
+}
