0e85090b4c8851e42df936851cf4f1a114be1293
angrykun <2545662859@qq.com>
2017-12-06 10:29:42 +0800

    .

---
+++ CSharpProjectNote/Util.Domain/EntityBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Domain
+{
+    /// <summary>
+    /// 领域实体
+    /// </summary>
+    public abstract class EntityBase<TKey>
+    {
+        /// <summary>
+        /// 初始化领域实体
+        /// </summary>
+        /// <param name="id"></param>
+        protected EntityBase(TKey id)
+        {
+            Id = id;
+        }
+        /// <summary>
+        /// 标识
+        /// </summary>
+        [Required]
+        public TKey Id { get; private set; }
+    }
+}
