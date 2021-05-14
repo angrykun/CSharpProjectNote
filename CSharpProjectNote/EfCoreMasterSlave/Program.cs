6ff2126a5b45b4b84351fa049ba64b5902c20f62
angrykun <2545662859@qq.com>
2021-05-14 11:59:43 +0800

    1.EFCore主从复制

---
+++ CSharpProjectNote/EfCoreMasterSlave/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EfCoreMasterSlave
+{
+    /// <summary>
+    /// EFCore 主从复制
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
